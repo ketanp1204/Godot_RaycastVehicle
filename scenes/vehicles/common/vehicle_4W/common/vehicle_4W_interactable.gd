64785b5537ff162da25b9934014d887f32bd5452
Ketan Patel <ktnp1204@gmail.com>
2024-07-12 14:07:48 +0200

    Added generic vehicle part interaction script

--- scenes/vehicles/common/vehicle_4W/common/vehicle_4W_interactable.gd
+++ scenes/vehicles/common/vehicle_4W/common/vehicle_4W_interactable.gd
@@ -3,29 +3,28 @@ extends Node3D
 enum INTERACT_STATE { OPEN, CLOSED }
 enum ROTATION_AXIS { X, Y, Z }
 
-
 @export_category("References")
-## The mesh resource that will be used for this door
-@export var door_mesh_res: Resource
+## The mesh resource that will be used for this Vehicle_4W part
+@export var mesh_res: Resource
 ## The NodePath for the MeshInstance3D node
-@export var door_mesh_node: NodePath
+@export var mesh_node: NodePath
 ## The NodePath for the external handle StaticBody3D node
 @export var external_handle_node: NodePath
 ## The NodePath for the internal handle StaticBody3D node
 @export var internal_handle_node: NodePath
 
 @export_category("Rotation Properties")
-## Maximum angle that the door opens to (degrees)
+## Maximum angle that this Vehicle_4W part opens to (degrees)
 @export var max_open_angle: float = 60.0
-## Door open speed (degrees per second)
+## Vehicle_4W part open speed (degrees per second)
 @export var open_speed: float = 90
-## Axis about which the door will rotate
+## Axis about which this Vehicle_4W will rotate
 @export var rotation_axis: ROTATION_AXIS
 ## Whether to use the negative value of MaxOpenAngle for the rotation
 @export var negative_rotation: bool = false
 
 
-var door_mesh: MeshInstance3D
+var mesh: MeshInstance3D
 var external_handle: StaticBody3D
 var internal_handle: StaticBody3D
 var state: INTERACT_STATE
@@ -33,26 +32,31 @@ var state: INTERACT_STATE
 
 
 func _ready() -> void:
+	# Get mesh nodes from the NodePath
+	mesh = get_node(mesh_node) as MeshInstance3D
 	
-	# Get nodes from the nodepaths
-	door_mesh = get_node(door_mesh_node) as MeshInstance3D
-	external_handle = get_node(external_handle_node) as StaticBody3D
-	internal_handle = get_node(internal_handle_node) as StaticBody3D
+	# Set the mesh of the MeshInstance3D if not set
+	if mesh.mesh == null:
+		if mesh_res == null:
+			print("ERROR @ Node " + name + ": Vehicle_4W part mesh not set in the inspector")
+			return
+		else:
+			mesh.mesh = mesh_res
+	
+	# Get external handle and connect clicked signal if set
+	if external_handle_node:
+		external_handle = get_node(external_handle_node) as StaticBody3D
+		external_handle.mouse_clicked.connect(toggle)
+	
+	# Get internal handle and connect clicked signal if set
+	if internal_handle_node:
+		internal_handle = get_node(internal_handle_node) as StaticBody3D
+		internal_handle.mouse_clicked.connect(toggle)
 	
 	# Set the default interaction state
 	state = INTERACT_STATE.CLOSED
 	
-	# Connect the mouse handle click signals
-	external_handle.mouse_clicked.connect(toggle)
-	internal_handle.mouse_clicked.connect(toggle)
 	
-	# Set the mesh of the MeshInstance3D if not set
-	if door_mesh.mesh == null:
-		if door_mesh_res == null:
-			print("ERROR @ Node " + name + ": Door mesh not set in the inspector")
-			return
-		else:
-			door_mesh.mesh = door_mesh_res
 
 
 func toggle() -> void:
@@ -86,4 +90,3 @@ func close() -> void:
 		0.0, 
 		anim_length).from_current()
 	state = INTERACT_STATE.CLOSED
-
