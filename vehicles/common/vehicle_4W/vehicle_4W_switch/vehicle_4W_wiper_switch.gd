5a1de2944e4a5e8beee0eca109d37d499462b54b
Ketan Patel <ktnp1204@gmail.com>
2024-07-17 21:13:40 +0200

    Added sound on wiper switch change

--- vehicles/common/vehicle_4W/vehicle_4W_switch/vehicle_4W_wiper_switch.gd
+++ vehicles/common/vehicle_4W/vehicle_4W_switch/vehicle_4W_wiper_switch.gd
@@ -6,8 +6,10 @@ const WIPER_CONTINUOUS_TO_RAPID_ANIM = "wiper_continuous_to_rapid"
 const WIPER_RAPID_TO_OFF_ANIM = "wiper_rapid_to_off"
 
 @export_category("References")
+@export var audio_stream_player_node: NodePath
 @export var anim_player_node: NodePath
 
+var audio_stream_player: AudioStreamPlayer3D
 var anim_player: AnimationPlayer
 
 var wiper_mode: GlobalEnums.WIPER_MODES = GlobalEnums.WIPER_MODES.OFF
@@ -16,8 +18,9 @@ var wiper_mode: GlobalEnums.WIPER_MODES = GlobalEnums.WIPER_MODES.OFF
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	# Get anim player from NodePath
+	# Get nodes from NodePath
+	if audio_stream_player_node:
+		audio_stream_player = get_node(audio_stream_player_node) as AudioStreamPlayer3D
 	if anim_player_node:
 		anim_player = get_node(anim_player_node) as AnimationPlayer
 	
@@ -33,6 +36,7 @@ func set_wiper_mode(mode: GlobalEnums.WIPER_MODES):
 	if wiper_mode == mode:
 		return
 	
+	SoundManager.play_light_switch_toggle(audio_stream_player)
 	if wiper_mode == GlobalEnums.WIPER_MODES.OFF \
 	and mode == GlobalEnums.WIPER_MODES.SINGLE:
 		anim_player.play(WIPER_OFF_TO_SINGLE_ANIM)
@@ -52,5 +56,6 @@ func set_wiper_mode(mode: GlobalEnums.WIPER_MODES):
 
 
 func wiper_single_to_continuous() -> void:
+	SoundManager.play_light_switch_toggle(audio_stream_player)
 	anim_player.play(WIPER_SINGLE_TO_CONTINUOUS_ANIM)
 	wiper_mode = GlobalEnums.WIPER_MODES.CONTINUOUS
